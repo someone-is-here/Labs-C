7270a1ffd698f5e33b49e11dac6a7b2004c28708
Tanusha <48770804+someone-is-here@users.noreply.github.com>
2021-04-04 21:02:59 +0300

    Update Header.h

--- Lab3C/Lab3C/Header.h
+++ Lab3C/Lab3C/Header.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifdef HEADER
+#define HEADER_H
 void printArray(int** array, int m, int n);
 int findSquare(int** array, int m, int n, int size);
 int** readArrayFromFile(char fileName[9]);
@@ -6,3 +7,4 @@ void clearArray(int** array, int m);
 int readM(char fileName[9]);
 int readN(char fileName[9]);
 void tests(char fileName[9], int size, int res);
+#endif 
